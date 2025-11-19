704468ae179db528ae818b7a4680f716046e1f99
Brandon-Butterbaugh <bjbutterbaugh@comcast.net>
2025-11-18 18:55:53 -0600

    bunch of database and sql stuff, along with handler function to create a user

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/Brandon-Butterbaugh/Chirbooty.git/internal/database"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	database       *database.Queries
+	platform       string
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -19,6 +28,22 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		fmt.Printf("Error writing response: %v\n", err)
+	}
+	dbQueries := database.New(db)
+
+	platform := os.Getenv("PLATFORM")
+	if platform == "" {
+		log.Fatal("PLATFORM must be set")
+	}
+
 	const filepathRoot = "."
 	const port = "8080"
 	mux := http.NewServeMux()
@@ -29,6 +54,8 @@ func main() {
 
 	cfg := &apiConfig{
 		fileserverHits: atomic.Int32{},
+		database:       dbQueries,
+		platform:       platform,
 	}
 	handler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
 
@@ -37,6 +64,7 @@ func main() {
 	mux.HandleFunc("GET /admin/metrics", cfg.Metrics)
 	mux.HandleFunc("POST /admin/reset", cfg.Reset)
 	mux.HandleFunc("POST /api/validate_chirp", validate)
+	mux.HandleFunc("POST /api/users", cfg.newUser)
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(serv.ListenAndServe())
--- reset.go
+++ reset.go
@@ -4,6 +4,20 @@ import (
 	"net/http"
 )
 
-func (cfg *apiConfig) Reset(w http.ResponseWriter, req *http.Request) {
-	cfg.fileserverHits.Store(0)
+func (cfg *apiConfig) Reset(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("Reset is only allowed in dev environment."))
+		return
+	} else {
+		cfg.fileserverHits.Store(0)
+		err := cfg.database.ResetUsers(r.Context())
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Failed to reset the database: " + err.Error()))
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hits reset to 0 and database reset to initial state."))
+	}
 }
